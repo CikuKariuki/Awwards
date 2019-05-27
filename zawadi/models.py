c8d0bc66ec38c968025746e17095eb4edc7b69b3
Wanjiku <Wanjiku>
2019-05-27 09:14:06 +0300

    created models for profile

--- zawadi/models.py
+++ zawadi/models.py
@@ -15,3 +15,41 @@ class Posts(models.Model):
     def search_by_posts(cls,search_term):
         posts = cls.objects.filter(posts__icontains=search_term)
         return posts
+
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    profile_picture = models.ImageField(upload_to='profiles/',default='default.png')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.CharField(max_length=50)
+    bio = HTMLField()
+    phone = models.CharField(max_length=15, blank=True)
+
+    class Meta:
+        ordering = ['first_name']
+        
+
+    def save_profile(self):
+        self.save()
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+
+    @classmethod
+    def get_by_id(cls,id):
+        profile = Profile.objects.get(user=id)
+        return profile
+
+    @classmethod
+    def filter_by_id(cls, id):
+        profile = Profile.objects.filter(user = id).first()
+        return profile
