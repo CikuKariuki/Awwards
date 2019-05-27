4d1f42f727471f24cfb7d3e7e6531689fc574b07
Wanjiku <Wanjiku>
2019-05-27 10:02:12 +0300

    Installed psycopg2 to link to database

--- awwards/settings.py
+++ awwards/settings.py
@@ -27,8 +27,8 @@ else:
        )
    }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
